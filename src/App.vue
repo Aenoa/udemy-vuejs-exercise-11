d3d31e31f3e1a805d7c23128a352fa523a525ee5
Hugo Regibo <hugo.regibo@havas.com>
2018-01-16 13:43:13 +0100

    init project + worked ex 1 & 2

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container ">
+        <form class="mtop">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
+                    <app-form v-if="!submitted" :form="myForm" />
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{myForm.user.fname + ' ' + myForm.user.lname}}</p>
+                        <p>Mail: {{myForm.user.mail}}</p>
+                        <p>Password: {{myForm.user.password}}</p>
+                        <p>Store in Database?: {{myForm.allowStorage}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Form from './components/Form/Form.vue';
+    export default {
+        data() {
+            return {
+                myForm: {
+                    user: {
+                        fname: '',
+                        lname: '',
+                        mail: '',
+                        password: '',
+                    },
+                    allowStorage: false,
+                    submitted: false,
+                }
+            }
+        },
+        components: {
+            appForm: Form,
+        }
+    }
+</script>
+
+<style scoped>
+.mtop {
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,49 @@
+<template>
+    <form>
+        <div class="form-group">
+            <label for="fname">First Name</label>
+            <input class="form-control" id="fname" v-model="form.user.fname" type="text" />
+        </div>
+        <div class="form-group">
+            <label for="lname">Last Name</label>
+            <input class="form-control" id="lname" v-model="form.user.lname" type="text" />
+        </div>
+
+        <div class="form-group">
+            <label for="eml">Email</label>
+            <input class="form-control" id="eml" v-model="form.user.mail" type="text" />
+        </div>
+
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" id="password" v-model="form.user.password" type="password" />
+        </div>
+
+        <div class="form-check">
+            <input class="form-check-input" id="storage" v-model="form.allowStorage" type="checkbox" />
+            <label class="form-check-label" for="storage">allow Database storage</label>
+        </div>
+
+        <button @click.prevent="form.submitted = true" class="btn btn-success">Submit this</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        props: {
+            form: {
+                required: true,
+                type: Object,
+            },
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style scoped>
+    button {
+        margin-top: 20px;
+    }
+</style>
